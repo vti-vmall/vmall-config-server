6d1329976d51b6e54cc4fc6599b835cf5e8b5996
tunk <nguyenkhactu1996@gmail.com>
2024-06-12 09:28:25 +0700

    Initial commit

---
+++ src/main/java/vn/edu/vti/vmall/vmallconfigserver/VmallConfigServerApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.vti.vmall.vmallconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmallConfigServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(VmallConfigServerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/vn/edu/vti/vmall/vmallconfigserver/VmallConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.vti.vmall.vmallconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VmallConfigServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
